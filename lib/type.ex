ea1f62265237fd91defde428405a350e4638e96e
Danilo Moura <danilolmoura@gmail.com>
2022-06-19 23:55:41 -0300

    feature: add types guard functions

---
+++ lib/type.ex
@@ -0,0 +1,9 @@
+defmodule Type do
+  @moduledoc """
+  Documentation for `Types`.
+  """
+
+  defguard validate_params(user) when is_binary(user)
+
+  defguard validate_params(user, amount, currency) when is_binary(user) and is_binary(currency) and is_float(amount) and amount > 0
+end
